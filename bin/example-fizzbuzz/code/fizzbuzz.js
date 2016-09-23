9a26709701660cf3519ef8a2c9f33de2e5600005
Arminas Žukauskas <ziogasas@gmail.com>
2016-09-23 12:39:48 +0300

    Clarifying fizzbuzz example

--- bin/example-fizzbuzz/code/fizzbuzz.js
+++ bin/example-fizzbuzz/code/fizzbuzz.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
+// Do not forget to do npm install first on this file folder
+
 var argv = require('yargs').argv,
     fizz = argv.fizz || 'Fizz',
     buzz = argv.buzz || 'Buzz',
     i;
 
-for (var i=1; i<100; i++) {
+for (i=1; i<100; i++) {
   console.log((i%3 ? '' : fizz) + (i%5 ? '' : buzz) || i);
 }
 
